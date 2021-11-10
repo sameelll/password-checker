f72c962e4cb5aec819ff58d5044c26f77a696960
Eren Akel <eren.akel.46@gmail.com>
2021-11-10 10:47:37 +0300

    errors have been fetched and the password has been converted to the sha1 form

--- check_my_pass.py
+++ check_my_pass.py
@@ -1,8 +1,20 @@
 # importing requests HTTP dictionary 
 import requests
-# SHA1 Hash method with the first 5 digit of a password 
-url = 'https://api.pwnedpasswords.com/range/' + 'CBFDA'
-# getting api
-res = requests.get(url)
-# response check(<Response [200]>)
-print(res)
+import hashlib
+# API request
+def request_api_data(query_char):
+    # SHA1 Hash method with the first 5 digit of a password 
+    url = 'https://api.pwnedpasswords.com/range/' + query_char    
+    # getting api
+    res = requests.get(url)
+    # response check(<Response [200]>)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res 
+    pass
+# Getting sha1 password
+def pwned_api_check(password):
+    # Converting the password to the sha1 hexadecimal form with upper case
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    return sha1password
+
