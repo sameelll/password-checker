8667fa4ae2c7ee1b17d88b64e770be1e16b0f655
Eren Akel <eren.akel.46@gmail.com>
2021-11-10 11:18:38 +0300

    the hashed form of password has been separated and the times that the password hacked has been taken

--- check_my_pass.py
+++ check_my_pass.py
@@ -11,10 +11,20 @@ def request_api_data(query_char):
     if res.status_code != 200:
         raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
     return res 
-    pass
+
+def get_password_leaks_count(hashes, hash_to_check): 
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        print(h, count)
+        
+
 # Getting sha1 password
 def pwned_api_check(password):
     # Converting the password to the sha1 hexadecimal form with upper case
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+pwned_api_check('123')
 
